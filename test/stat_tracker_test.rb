f34cfc294495fc6a4532637cc8469601dc18c574
Sam Yeo <yeosamuel95@gmail.com>
2020-11-02 16:19:49 -0700

    Remove commented methods and tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -12,61 +12,6 @@ class StatTrackerTest < MiniTest::Test
     @stat_tracker = StatTracker.from_csv(locations)
   end
 
-# Game Statistics Methods
-  # def test_highest_total_score
-  #   assert_equal 11, @stat_tracker.highest_total_score
-  # end
-
-  
-  # def test_lowest_total_score
-  #   assert_equal 1, @stat_tracker.lowest_total_score
-  # end
-  
-  # def test_percentage_home_wins
-  #   assert_equal 59.85, @stat_tracker.percentage_home_wins
-  # end
-  
-  # def test_percentage_visitor_wins
-  #   assert_equal 36.5, @stat_tracker.percentage_visitor_wins
-  # end
-  
-  #def test_calc_percentage
-    #assert_equal 40.00, @stat_tracker.calc_percentage(2, 5)
-  #end
-  
-  #def test_percentage_ties
-    #assert_equal 3.65, @stat_tracker.percentage_ties
-  #end
-  
-  #def test_count_of_games_by_season
-    #hash = {
-      #"20122013" => 57,
-      #"20132014" => 6,
-      #"20142015" => 17,
-      #"20152016" => 16,
-      #"20162017" => 4
-    #}
-    #assert_equal hash, @stat_tracker.count_of_games_by_season
-  #end
-  
-  # def test_average_goals_per_game
-  #   avg = 3.95
-    
-  #   assert_equal avg, @stat_tracker.average_goals_per_game
-  # end
-  
-  # def test_average_goals_by_season
-  #   hash = {
-  #     "20122013" => 3.86,
-  #     "20132014" => 4.33,
-  #     "20142015" => 4.00,
-  #     "20152016" => 3.88,
-  #     "20162017" => 4.75
-  #   }
-    
-  #   assert_equal hash, @stat_tracker.average_goals_by_season
-  # end
-
 # League Statistics Methods
   def test_count_of_teams
     assert_equal 32, @stat_tracker.count_of_teams
