e7bff951dba6ac80b5cd602c586c510761a99b05
Saundra <saundracatalina@gmail.com>
2020-11-02 16:48:45 -0700

    Add test_it_exists_and_has_attributes

--- test/teams_manager_test.rb
+++ test/teams_manager_test.rb
@@ -6,4 +6,10 @@ class GamesManagerTest < Minitest::Test
     parent = nil
     @teams_manager = TeamsManager.get_data(location, parent)
   end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of TeamsManager, @teams_manager
+    assert_equal 32, @teams_manager.teams.length
+    assert_nil @teams_manager.parent
+  end
 end
